ef447ada545f5ec5491dd346d10e7b768e6ca5f6
mynamo101 <mynamo101@gmail.com>
2025-03-12 06:16:59 +0800

    123

--- docs/.vuepress/notes/zh/FMOD-Guide.ts
+++ docs/.vuepress/notes/zh/FMOD-Guide.ts
@@ -3,5 +3,22 @@ import { defineNoteConfig } from 'vuepress-theme-plume'
 export const FMODGuide = defineNoteConfig({
   dir: 'zh/Guide/FMOD',
   link: '/FMOD/',
-  sidebar:'auto',
+  sidebar:[
+    {
+      text: '从这里开始',
+      collapsed: false,
+      icon: 'carbon:idea',
+      items: [
+        '介绍',
+        '安装与使用',
+        '项目结构',
+        '编写文章',
+        '博客',
+        '知识笔记',
+        '国际化',
+        '部署',
+        '构建优化',
+      ],
+    },
+  ],
 })
