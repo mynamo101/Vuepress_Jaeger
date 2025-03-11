ce2e13929666232bca2e8aaf786c1e6d55cf0338
mynamo101 <mynamo101@gmail.com>
2025-03-11 15:05:23 +0800

    Test

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,6 @@
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
-import notes from './notes'
 
 export default defineUserConfig({
   base: '/',
