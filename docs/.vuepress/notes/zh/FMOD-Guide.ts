e414b947ed915eb03b598beed54c96bca89694a1
mynamo101 <mynamo101@gmail.com>
2025-03-12 05:53:35 +0800

    234

--- docs/.vuepress/notes/zh/FMOD-Guide.ts
+++ docs/.vuepress/notes/zh/FMOD-Guide.ts
@@ -1,7 +1,7 @@
 import { defineNoteConfig } from 'vuepress-theme-plume'
 
 export const FMODGuide = defineNoteConfig({
-  dir: 'Guide/FMOD',
+  dir: '/zh/Guide/FMOD',
   link: '/FMOD/',
   sidebar:'auto',
 })
--- docs/.vuepress/notes/zh/index.ts
+++ docs/.vuepress/notes/zh/index.ts
@@ -2,7 +2,7 @@ import { defineNotesConfig } from 'vuepress-theme-plume'
 import { FMODGuide } from './FMOD-Guide' //這邊添加需要的目録
 export const zhNotes = defineNotesConfig({
   dir: 'notes',
-  link: '/zh/',
+  link: '/',
   notes: [
     FMODGuide,
   ],
