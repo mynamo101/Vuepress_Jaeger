587f3c8c1c0c6b4eb1240f509aea6e7fcca68bf3
mynamo101 <mynamo101@gmail.com>
2025-03-11 19:16:38 +0800

    測試

--- docs/.vuepress/notes/zh/FMOD-Guide.ts
+++ docs/.vuepress/notes/zh/FMOD-Guide.ts
@@ -1,7 +1,7 @@
 import { defineNoteConfig } from 'vuepress-theme-plume'
 
 export const FMODGuide = defineNoteConfig({
-  dir: 'FMOD',
-  link: '/',
+  dir: 'Guide/FMOD',
+  link: '/FMOD/',
   sidebar:'auto',
 })
