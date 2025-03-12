f649aa86962834bc2461bce312616acc500f2be6
mynamo101 <mynamo101@gmail.com>
2025-03-12 15:08:11 +0800

    大頭貼

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -4,10 +4,10 @@ import { enNavbar, zhNavbar } from './navbar.js'
 import { enNotes, zhNotes } from './notes/index.js'
 
 export default defineThemeConfig({
-  logo: '/.vuepress/public/Images/Avatar.jpg',
+  logo: '/docs/.vuepress/public/Images/Avatar.jpg',
 
   profile: {
-    avatar: '/.vuepress/public/Images/Avatar.jpg',
+    avatar: '/docs/.vuepress/public/Images/Avatar.jpg',
     name: 'MingYen Cheng',
     description: 'Sound & Music Lover',
     location: 'Hsinchu, Taiwan',
