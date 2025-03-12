a8185db337801099720c9ad01a4f4ff6bec31cd4
mynamo101 <mynamo101@gmail.com>
2025-03-12 17:39:53 +0800

    修改導航欄

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -11,49 +11,20 @@ export const zhNavbar = defineNavbarConfig([
     ],
     activeMatch: '^/FMOD/',
   },
-  {
-    text: '配置',
-    icon: 'icon-park-outline:setting-two',
-    link: '/notes/theme/config/配置说明.md',
-    activeMatch: '^/config/',
-  },
+
   {
     text: '博客',
     link: '/blog/',
     icon: 'material-symbols:article-outline',
     activeMatch: '^/(blog|article)/',
   },
+  
   {
     text: '案例',
     link: '/demos/',
     icon: 'map:wind-surfing',
   },
-  {
-    text: '更多',
-    icon: 'icon-park-outline:more-three',
-    items: [
-      { text: '常见问题', link: '/faq/', icon: 'wpf:faq' },
-      { text: '喝杯奶茶', link: '/sponsor/', icon: 'line-md:coffee-loop' },
-      { text: '主题工具', link: '/tools/', icon: 'jam:tools' },
-      { text: '友情链接', link: '/friends/', icon: 'carbon:friendship' },
-      {
-        text: 'Vuepress',
-        icon: 'logos:vue',
-        items: [
-          { text: '官方文档', link: 'https://v2.vuepress.vuejs.org', icon: 'logos:vue' },
-          { text: '生态系统', link: 'https://ecosystem.vuejs.press/', icon: 'logos:vue' },
-        ],
-      },
-    ],
-  },
-  {
-    text: `${version}`,
-    icon: 'codicon:versions',
-    items: [
-      { text: '更新日志', link: '/changelog/' },
-      { text: '参与贡献', link: '/contributing/' },
-    ],
-  },
+
 ])
 
 export const enNavbar = defineNavbarConfig([
