032129ba0f316329d439b1d1383f82a0ec95e4cc
mynamo101 <mynamo101@gmail.com>
2025-04-05 01:54:55 +0800

    提交官方文檔版本

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,16 +2,16 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 import { version } from '../../package.json'
 
 export const zhNavbar = defineNavbarConfig([
-  {
-    text: '聲音引擎',
-    icon: 'icon-park-outline:guide-board',
-    items: [
-      { text: '基礎知識', link: '/notes/Readme.md', icon: 'wpf:books' },
-      { text: 'FMOD', link: '/notes/FMOD/1.開始準備/介紹.md', icon: 'wpf:faq' },
-      // { text: 'Wwise', link: '/notes/Wwise/1.開始準備/介紹.md', icon: 'line-md:coffee-loop' },
-    ],
-    activeMatch: '^/(FMOD|Wwise)/',
-  },
+  // {
+  //   text: '聲音引擎',
+  //   icon: 'icon-park-outline:guide-board',
+  //   items: [
+  //     { text: '基礎知識', link: '/notes/Readme.md', icon: 'wpf:books' },
+  //     { text: 'FMOD', link: '/notes/FMOD/1.開始準備/介紹.md', icon: 'wpf:faq' },
+  //     // { text: 'Wwise', link: '/notes/Wwise/1.開始準備/介紹.md', icon: 'line-md:coffee-loop' },
+  //   ],
+  //   activeMatch: '^/(FMOD|Wwise)/',
+  // },
   {
     text: '服務說明',
     icon: 'icon-park-outline:expand-down',
@@ -23,24 +23,24 @@ export const zhNavbar = defineNavbarConfig([
   //   link: '/notes/SoundTools/1.Intro.md',
   //   icon: 'map:wind-surfing',
   // },
-  {
-    text: '博客',
-    link: '/blog/',
-    icon: 'material-symbols:article-outline',
-    activeMatch: '^/(blog|article)/',
-  },
-  {
-    text: '查看更多',
-    icon: 'icon-park-outline:more-three',
-    items: [
-      {
-        items: [
-          { text: '友情鏈接', link: '/Friends.md' },
-          { text: 'Plume Theme', link: 'https://theme-plume.vuejs.press/' },
-        ],
-      },
-    ],
-  },
+  // {
+  //   text: '博客',
+  //   link: '/blog/',
+  //   icon: 'material-symbols:article-outline',
+  //   activeMatch: '^/(blog|article)/',
+  // },
+  // {
+  //   text: '查看更多',
+  //   icon: 'icon-park-outline:more-three',
+  //   items: [
+  //     {
+  //       items: [
+  //         { text: '友情鏈接', link: '/Friends.md' },
+  //         { text: 'Plume Theme', link: 'https://theme-plume.vuejs.press/' },
+  //       ],
+  //     },
+  //   ],
+  // },
 ])
 
 export const enNavbar = defineNavbarConfig([
